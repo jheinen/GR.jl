ca0112fa138bcd4bb1e5b1dfc44c9bc916db60a1
Josef Heinen <j.heinen@fz-juelich.de>
2015-05-22 18:43:46 +0200

    Only download if GR does not exist

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,13 @@
-const version = v"0.13.0"
-const gr = "gr-$version"
-const os = OS_NAME
-const tarball = "gr-$version-$os.tar.gz"
-
-if !isfile("downloads/$tarball")
+have_env = "GRDIR" in keys(ENV)
+if !have_env && !isdir("/usr/local/gr") && !isdir(joinpath(homedir(),"gr"))
+  const version = v"0.13.0"
+  const gr = "gr-$version"
+  const os = OS_NAME
+  const tarball = "gr-$version-$os.tar.gz"
+  if !isfile("downloads/$tarball")
     info("Downloading pre-compiled GR $version binary")
     mkpath("downloads")
     download("http://gr-framework.org/downloads/$tarball", "downloads/$tarball")
     run(`tar xzf downloads/$tarball`)
+  end
 end
