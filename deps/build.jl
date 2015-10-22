bef725ca75d87a331fdc68c84f4e36dbde02b09b
Josef Heinen <j.heinen@fz-juelich.de>
2015-10-22 08:24:12 +0200

    Support architecture specific tar balls

--- deps/build.jl
+++ deps/build.jl
@@ -1,9 +1,10 @@
 have_env = "GRDIR" in keys(ENV)
 if !have_env && !isdir("/usr/local/gr") && !isdir(joinpath(homedir(),"gr"))
-  const version = v"0.16.0"
+  const version = v"0.17.1"
   const gr = "gr-$version"
   const os = OS_NAME
-  const tarball = "gr-$version-$os.tar.gz"
+  const arch = Sys.ARCH
+  const tarball = "gr-$version-$os-$arch.tar.gz"
   if !isfile("downloads/$tarball")
     info("Downloading pre-compiled GR $version binary")
     mkpath("downloads")
