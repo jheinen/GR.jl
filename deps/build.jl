889f9d07ba02a564e7b2f33e11d005d328bbdb8b
Josef Heinen <j.heinen@fz-juelich.de>
2016-02-01 17:26:13 +0100

    Fixed build problem on Windows

--- deps/build.jl
+++ deps/build.jl
@@ -4,11 +4,19 @@ if !have_env && !isdir("/usr/local/gr") && !isdir(joinpath(homedir(),"gr"))
   const gr = "gr-$version"
   const os = OS_NAME
   const arch = Sys.ARCH
-  const tarball = "gr-$version-$os-$arch.tar.gz"
+  tarball = "gr-$version-$os-$arch.tar.gz"
   if !isfile("downloads/$tarball")
     info("Downloading pre-compiled GR $version binary")
     mkpath("downloads")
     download("http://gr-framework.org/downloads/$tarball", "downloads/$tarball")
-    run(`tar xzf downloads/$tarball`)
+    @windows_only begin
+      success(`$JULIA_HOME/7z x downloads/$tarball -y`)
+      tarball = tarball[1:end-3]
+      success(`$JULIA_HOME/7z x $tarball -y -ttar`)
+      rm("$tarball")
+    end
+    @unix_only begin
+      run(`tar xzf downloads/$tarball`)
+    end
   end
 end
