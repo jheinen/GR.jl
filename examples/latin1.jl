7d91a093cf1c49b1dd776dc91744bc166b3b60f1
Josef Heinen <j.heinen@fz-juelich.de>
2016-03-02 08:03:25 +0100

    Added example to demonstrate Latin-1 text output

---
+++ examples/latin1.jl
@@ -0,0 +1,21 @@
+import GR
+
+GR.selntran(0)
+GR.settextfontprec(2, 0)
+GR.setcharheight(0.024)
+
+GR.text(0.05, 0.85, " !\"#\$%&'()*+,-./")
+GR.text(0.05, 0.80, "0123456789:;<=>?")
+GR.text(0.05, 0.75, "@ABCDEFGHIJKLMNO")
+GR.text(0.05, 0.70, "PQRSTUVWXYZ[\\]^_")
+GR.text(0.05, 0.65, "`abcdefghijklmno")
+GR.text(0.05, 0.60, "pqrstuvwxyz{|}~ ")
+
+GR.text(0.5, 0.85, " ¡¢£¤¥¦§¨©ª«¬­®¯")
+GR.text(0.5, 0.80, "°±²³´µ¶·¸¹º»¼½¾¿")
+GR.text(0.5, 0.75, "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏ")
+GR.text(0.5, 0.70, "ÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞß")
+GR.text(0.5, 0.65, "àáâãäåæçèéêëìíîï")
+GR.text(0.5, 0.60, "ðñòóôõö÷øùúûüýþÿ")
+
+GR.updatews()
