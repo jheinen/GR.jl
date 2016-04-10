de43896b83e4d11372045e708d7f458db861a46f
Josef Heinen <j.heinen@fz-juelich.de>
2016-04-10 10:53:27 +0200

    Ported spectrogram example to the new architecture

--- examples/spectogram.jl
+++ examples/spectogram.jl
@@ -1,27 +1,40 @@
-import AudioIO
-const pa = AudioIO
+if VERSION < v"0.4-"
+  import AudioIO
+  const pa = AudioIO
+else
+  import PortAudio, LibSndFile
+end
 import GR
 
 function play()
-  f = pa.open("Monty_Python.wav")
-  data = read(f)
-  close(f)
-  
-  pa.Pa_Initialize()
-  stream = pa.Pa_OpenDefaultStream(0, 1, pa.paInt16, 44100.0, 1024)
-  pa.Pa_StartStream(stream)
-  
+  if VERSION < v"0.4-"
+    f = pa.open("Monty_Python.wav")
+    data = read(f)
+    close(f)
+
+    pa.Pa_Initialize()
+    stream = pa.Pa_OpenDefaultStream(0, 1, pa.paInt16, 44100.0, 1024)
+    pa.Pa_StartStream(stream)
+  else
+    data = LibSndFile.load("Monty_Python.wav")
+    stream = PortAudio.PortAudioStream(0, 1, bufsize=1024)
+  end
   spectrum = zeros(Int32, 250, 250)
-  
+
   start = 1
   while start + 1024 < length(data)
     amplitudes = data[start:start+1024]
-
-    pa.Pa_WriteStream(stream, amplitudes)
     start += 1024
-  
-    power = log(abs(fft(amplitudes)) / 22050.0 + 1) * 50
-    spectrum[:, 1] = int(power[1:250]) + 1000
+
+    if VERSION < v"0.4-"
+      pa.Pa_WriteStream(stream, amplitudes)
+      amplitudes /= 22050.0
+    else
+      PortAudio.write(stream, amplitudes)
+    end
+
+    power = log(abs(fft(amplitudes)) + 1) * 50
+    spectrum[:, 1] = round(Int, power[1:250]) + 1000
     spectrum = circshift(spectrum, [0, -1])
 
     produce(spectrum)
