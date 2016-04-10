768dd33f772e136c73979847192b75758d8482c6
Josef Heinen <j.heinen@fz-juelich.de>
2016-04-10 10:51:03 +0200

    Added real-time spectrogram plot example

---
+++ examples/spectrum.jl
@@ -0,0 +1,16 @@
+# Plot a real-time spectrogram
+# see https://github.com/JuliaAudio/PortAudio.jl
+
+using GR, PortAudio, SampledSignals
+
+const N = 1024
+const stream = PortAudioStream(1, 0, bufsize=N)
+const buf = read(stream, N)
+const fmin = 0Hz
+const fmax = 10000Hz
+const fs = Float32[float(f) for f in domain(fft(buf)[fmin..fmax])]
+
+while true
+    read!(stream, buf)
+    plot(fs, abs(fft(buf)[fmin..fmax]), xlim=(fs[1],fs[end]), ylim=(0,100))
+end
