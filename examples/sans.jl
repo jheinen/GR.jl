6f09b61555ae5fb1e15ea65627fa73856655e21c
Josef Heinen <j.heinen@fz-juelich.de>
2016-04-10 12:35:58 +0200

    Use (new) surface() function

--- examples/sans.jl
+++ examples/sans.jl
@@ -4,7 +4,6 @@ z = Z = readdlm("sans.dat")
 G = [ exp(-x^2 -y^2) for x in linspace(-1.5, 1.5, 128), y in linspace(-1.5, 1.5, 128) ]
 
 for t = 0:500
-    plot3d(z, contours=false, accelerate=true, ztitle="Time: $t s",
-           colormap=GR.COLORMAP_COOLWARM)
+    surface(z, title="Time: $t s")
     z += 0.05 * Z .* G .* rand((128, 128))
 end
