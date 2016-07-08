e0f2e000f5255e290a0e1a7df5f38ea497e85eb2
Josef Heinen <j.heinen@fz-juelich.de>
2016-07-08 08:04:34 +0200

    Fixed readbytes deprecation

--- src/GR.jl
+++ src/GR.jl
@@ -1354,10 +1354,10 @@ function show()
         content = PNG(_readfile("gks.png"))
         return content
     elseif mime_type == "mov"
-        content = HTML(string("""<video autoplay controls><source type="video/mp4" src="data:video/mp4;base64,""", base64encode(open(readbytes,"gks.mov")),""""></video>"""))
+        content = HTML(string("""<video autoplay controls><source type="video/mp4" src="data:video/mp4;base64,""", base64encode(open(read,"gks.mov")),""""></video>"""))
         return content
     elseif mime_type == "iterm"
-        content = string("\033]1337;File=inline=1;height=24;preserveAspectRatio=0:", base64encode(open(readbytes,"gks.pdf")), "\a")
+        content = string("\033]1337;File=inline=1;height=24;preserveAspectRatio=0:", base64encode(open(read,"gks.pdf")), "\a")
         if figure_count != None
             figure_count += 1
             (figure_count > 1) && print("\e[24A")
