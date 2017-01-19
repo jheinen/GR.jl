b3eae6227476b649a66de021ae2fcc0b72e4fc15
Josef Heinen <j.heinen@fz-juelich.de>
2017-01-19 08:55:19 +0100

    Escape macro arguments for v0.6 compatibility

--- src/gr3.jl
+++ src/gr3.jl
@@ -11,7 +11,7 @@ macro triplet(t)
 end
 
 macro ArrayToVector(ctype, data)
-    return :( convert(Vector{$ctype}, vec($data)) )
+    return :( convert(Vector{$(esc(ctype))}, vec($(esc(data)))) )
 end
 
 type PNG
