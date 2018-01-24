4e6e04a963c0a3298ad91ebeaf494bc71722680e
Josef Heinen <j.heinen@fz-juelich.de>
2018-01-24 13:07:52 +0100

    Change test script for compatibility with v0.7

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 import GR
-using Base.Test
+
+@static if VERSION < v"0.7.0-DEV.2005"
+    using Base.Test
+else
+    using Test
+end
+@static if VERSION >= v"0.7.0-DEV.3406"
+    using Random
+end
 
 @test GR.tick(1.2,3.14) == 0.5
 
