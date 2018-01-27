a106dfa23b3b42ddeb84021207acf414c282a2f6
Josef Heinen <j.heinen@fz-juelich.de>
2018-01-27 16:11:26 +0100

    Added 'inqmathtex()' function

--- src/GR.jl
+++ src/GR.jl
@@ -2717,6 +2717,16 @@ function mathtex(x::Real, y::Real, string)
         x, y, string)
 end
 
+function inqmathtex(x, y, string)
+  tbx = Cdouble[0, 0, 0, 0]
+  tby = Cdouble[0, 0, 0, 0]
+  ccall( (:gr_inqmathtex, libGR),
+        Nothing,
+        (Float64, Float64, Ptr{UInt8}, Ptr{Cdouble}, Ptr{Cdouble}),
+        x, y, string, tbx, tby)
+  return tbx, tby
+end
+
 ASF_BUNDLED = 0
 ASF_INDIVIDUAL = 1
 
