2f18494a2bddee2b0fe69f1f9391e84214d69ff8
Josef Heinen <j.heinen@fz-juelich.de>
2018-04-13 11:52:48 +0200

    Set GR version to v0.31.0

--- deps/build.jl
+++ deps/build.jl
@@ -16,7 +16,7 @@ function check_grdir()
 end
 
 function get_version()
-    version = v"0.30.2"
+    version = v"0.31.0"
     try
         v = Pkg.installed("GR")
         if string(v)[end:end] == "+"
