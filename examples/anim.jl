8860e366865b6b0bb1943485bc2ebc3665129d5f
Josef Heinen <j.heinen@fz-juelich.de>
2018-12-17 08:34:00 +0100

    Replace tic(), toc() function calls

--- examples/anim.jl
+++ examples/anim.jl
@@ -10,26 +10,28 @@ import PyPlot
 
 x = collect(0:0.01:2*pi)
 
-tic()
 line, = PyPlot.plot(x, sin.(x))
+t = time_ns()
 for i = 1:100
-    line[:set_ydata](sin.(x + i / 10.0))
+    line[:set_ydata](sin.(x .+ i / 10.0))
     PyPlot.draw()
     PyPlot.pause(0.0001)
 end
 
-fps_mpl = round(100 / toq())
+fps_mpl = round(100 / (1e-9 * (time_ns() - t)))
 println("fps (mpl): ", fps_mpl)
 
 import GR
 
-tic()
+GR.plot(x, sin.(x))
+t = time_ns()
 for i = 1:100
-    GR.plot(x, sin.(x + i / 10.0))
+    GR.plot(x, sin.(x .+ i / 10.0))
     sleep(0.0001) # unnecessary
 end
 
-fps_gr = round(100 / toq())
+fps_gr = round(100 / (1e-9 * (time_ns() - t)))
 println("fps  (GR): ", fps_gr)
 
+using Printf
 println(@sprintf("  speedup: %6.1f",  float(fps_gr) / fps_mpl))
