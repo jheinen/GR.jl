bdccfe1341a3958e722fa364180c8dc336da47bb
Josef Heinen <j.heinen@fz-juelich.de>
2018-12-17 17:15:57 +0100

    Upgrade animation and notebook examples

--- examples/anim.jl
+++ examples/anim.jl
@@ -2,36 +2,47 @@
 #
 # These are the results on a MacBook Pro 2,6 GHz Intel Core i5::
 #
-#     fps (mpl):   27
-#     fps  (GR):  775
-#       speedup:   28.7
+# MPL:   10 fps  GR: 1400 fps  => speedup:  140.0
 
 import PyPlot
-
-x = collect(0:0.01:2*pi)
-
-line, = PyPlot.plot(x, sin.(x))
-t = time_ns()
-for i = 1:100
-    line[:set_ydata](sin.(x .+ i / 10.0))
-    PyPlot.draw()
-    PyPlot.pause(0.0001)
-end
-
-fps_mpl = round(100 / (1e-9 * (time_ns() - t)))
-println("fps (mpl): ", fps_mpl)
-
 import GR
+ENV["GKSwstype"] = "gksqt"
 
-GR.plot(x, sin.(x))
-t = time_ns()
-for i = 1:100
-    GR.plot(x, sin.(x .+ i / 10.0))
-    sleep(0.0001) # unnecessary
+function mpl()
+    x = collect(0:0.01:2*pi)
+
+    PyPlot.plot(x, sin.(x))
+    t = time_ns()
+    for i = 1:100
+        PyPlot.cla()
+        PyPlot.plot(x .+ i / 10.0, sin.(x .+ i / 10.0))
+        PyPlot.pause(0.0001)
+    end
+
+    fps = round(Int64, 100 / (1e-9 * (time_ns() - t)))
 end
 
-fps_gr = round(100 / (1e-9 * (time_ns() - t)))
-println("fps  (GR): ", fps_gr)
+function gr()
+    x = collect(0:0.01:2*pi)
+
+    GR.plot(x, sin.(x))
+    t = time_ns()
+    for i = 1:100
+        GR.plot(x .+ i / 10.0, sin.(x .+ i / 10.0))
+    end
+
+    fps = round(Int64, 100 / (1e-9 * (time_ns() - t)))
+end
 
 using Printf
-println(@sprintf("  speedup: %6.1f",  float(fps_gr) / fps_mpl))
+
+for i in 1:10
+    fps_mpl = mpl()
+    fps_gr = gr()
+    speedup = float(fps_gr) / fps_mpl
+
+    @printf("MPL: %4d fps  GR: %4d fps  => speedup: %6.1f\r",
+            fps_mpl, fps_gr, speedup)
+end
+
+@printf("\n")
