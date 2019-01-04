55e3231701370424eb5e84f67a12969ff6757009
Josef Heinen <j.heinen@fz-juelich.de>
2019-01-04 17:41:34 +0100

    Added workaround for text problems in Gtk

--- examples/gtk_ex.jl
+++ examples/gtk_ex.jl
@@ -7,11 +7,13 @@ function paint(w)
     h = Gtk.height(w)
     w = Gtk.width(w)
 
-#    Gtk.select_font_face(ctx, "Sans",
-#                         Cairo.FONT_SLANT_NORMAL, Cairo.FONT_WEIGHT_NORMAL);
-#    Gtk.move_to(ctx, 15, 15)
-#    Gtk.set_font_size(ctx, 14)
-#    Gtk.show_text(ctx, "Contour Plot using Gtk ...")
+    if !Sys.isapple()
+        Gtk.select_font_face(ctx, "Sans",
+                             Cairo.FONT_SLANT_NORMAL, Cairo.FONT_WEIGHT_NORMAL);
+        Gtk.move_to(ctx, 15, 15)
+        Gtk.set_font_size(ctx, 14)
+        Gtk.show_text(ctx, "Contour Plot using Gtk ...")
+    end
 
     ENV["GKS_WSTYPE"] = "142"
     ENV["GKSconid"] = @sprintf("%lu", UInt64(ctx.ptr))
@@ -33,7 +35,8 @@ function paint(w)
     levels = LinRange(-0.5, 0.5, 20)
     GR.contourf(x, y, levels, z, 0)
     GR.polymarker(xd, yd)
-#    GR.axes(0.25, 0.25, -2, -2, 2, 2, 0.01)
+    GR.settextfontprec(1, 2)
+    GR.axes(0.25, 0.25, -2, -2, 2, 2, 0.01)
 
     GR.updatews()
 end
@@ -45,7 +48,10 @@ Gtk.push!(win, canvas)
 Gtk.draw(paint, canvas)
 Gtk.showall(win)
 
-signal_connect(win, :destroy) do widget
-    Gtk.gtk_quit()
+if !isinteractive()
+    c = Condition()
+    signal_connect(win, :destroy) do widget
+        notify(c)
+    end
+    wait(c)
 end
-Gtk.gtk_main()
