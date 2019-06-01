53e726876e489ed75e65fb12077e000031d74db7
Josef Heinen <j.heinen@fz-juelich.de>
2019-06-01 07:33:17 +0200

    Added 'polarcellarray' method

--- src/GR.jl
+++ src/GR.jl
@@ -39,6 +39,7 @@ export
   inqtext,
   fillarea,
   cellarray,
+  polarcellarray,
   gdp,
   spline,
   gridit,
@@ -664,6 +665,48 @@ function cellarray(xmin::Real, xmax::Real, ymin::Real, ymax::Real, dimx::Int, di
         Nothing,
         (Float64, Float64, Float64, Float64, Int32, Int32, Int32, Int32, Int32, Int32, Ptr{Int32}),
         xmin, xmax, ymin, ymax, dimx, dimy, 1, 1, dimx, dimy, convert(Vector{Int32}, color))
+end
+
+"""
+    polarcellarray(xorg::Real, yorg::Real, phimin::Real, phimax::Real, rmin::Real, rmax::Real, imphi::Int, dimr::Int, color)
+
+Display a two dimensional color index array mapped to a disk using polar
+coordinates.
+
+**Parameters:**
+
+`xorg` :
+    X coordinate of the disk center in world coordinates
+`yorg` :
+    Y coordinate of the disk center in world coordinates
+`phimin` :
+    start angle of the disk sector in degrees
+`phimax` :
+    end angle of the disk sector in degrees
+`rmin` :
+    inner radius of the punctured disk in world coordinates
+`rmax` :
+    outer radius of the punctured disk in world coordinates
+`dimiphi`, `dimr` :
+    Phi (X) and iR (Y) dimension of the color index array
+`color` :
+    Color index array
+
+The two dimensional color index array is mapped to the resulting image by
+interpreting the X-axis of the array as the angle and the Y-axis as the raidus.
+The center point of the resulting disk is located at `xorg`, `yorg` and the
+radius of the disk is `rmax`.
+
+"""
+function polarcellarray(xorg::Real, yorg::Real, phimin::Real, phimax::Real, rmin::Real, rmax::Real,
+                        dimphi::Int, dimr::Int, color)
+  if ndims(color) == 2
+    color = reshape(color, dimphi * dimr)
+  end
+  ccall( (:gr_polarcellarray, libGR),
+        Nothing,
+        (Float64, Float64, Float64, Float64, Float64, Float64, Int32, Int32, Int32, Int32, Int32, Int32, Ptr{Int32}),
+        xorg, yorg, phimin, phimax, rmin, rmax, dimphi, dimr, 1, 1, dimphi, dimr, convert(Vector{Int32}, color))
 end
 
 """
