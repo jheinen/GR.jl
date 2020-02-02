a2317a658c414c47e90550de2998d2e78078ca91
Josef Heinen <j.heinen@fz-juelich.de>
2020-02-02 17:03:28 +0100

    Correct peaks() function for MATLAB compatibility

--- src/jlgr.jl
+++ src/jlgr.jl
@@ -2449,10 +2449,9 @@ function meshgrid(vx, vy, vz)
 end
 
 function peaks(n=49)
-    x = LinRange(-2.5, 2.5, n)
-    y = x
-    x, y = meshgrid(x, y)
-    3 * (1 .- x).^2 .* exp.(-(x.^2) .- (y.+1).^2) .- 10*(x/5 .- x.^3 .- y.^5) .* exp.(-x.^2 .- y.^2) .- 1/3 * exp.(-(x.+1).^2 .- y.^2)
+    x = LinRange(-3, 3, n)
+    y = LinRange(-3, 3, n)
+    3 * (1 .- x').^2 .* exp.(-(x'.^2) .- (y.+1).^2) .- 10*(x'/5 .- x'.^3 .- y.^5) .* exp.(-x'.^2 .- y.^2) .- 1/3 * exp.(-(x'.+1).^2 .- y.^2)
 end
 
 """
