5a1d888b58aaf42ba3819ca6caa10fce08b04ece
Florian Rhiem <f.rhiem@fz-juelich.de>
2020-03-18 14:02:12 +0100

    Fix rst parsing error

--- src/GR.jl
+++ src/GR.jl
@@ -734,8 +734,8 @@ end
 `color` :
     Color index array
 
-The values for `x` and `y` are in world coordinates. `x` must contain `dimx`+1 elements
-and `y` must contain `dimy`+1 elements. The elements i and i+1 are respectively the edges
+The values for `x` and `y` are in world coordinates. `x` must contain `dimx` + 1 elements
+and `y` must contain `dimy` + 1 elements. The elements i and i+1 are respectively the edges
 of the i-th cell in X and Y direction.
 
 """
