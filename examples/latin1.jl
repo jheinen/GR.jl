1ed22e862738d9059d066187406641842eafba63
Josef Heinen <j.heinen@fz-juelich.de>
2020-04-21 16:43:56 +0200

    Expand the Latin-1 example script

--- examples/latin1.jl
+++ examples/latin1.jl
@@ -1,21 +1,30 @@
 import GR
 
 GR.selntran(0)
-GR.settextfontprec(2, 0)
 GR.setcharheight(0.024)
 
-GR.text(0.05, 0.85, " !\"#\$%&'()*+,-./")
-GR.text(0.05, 0.80, "0123456789:;<=>?")
-GR.text(0.05, 0.75, "@ABCDEFGHIJKLMNO")
-GR.text(0.05, 0.70, "PQRSTUVWXYZ[\\]^_")
-GR.text(0.05, 0.65, "`abcdefghijklmno")
-GR.text(0.05, 0.60, "pqrstuvwxyz{|}~ ")
+function main()
+    GR.settextfontprec(2, 0)
+    y = 0
+    for i in 0:1
+        GR.text(0.05, 0.85-y, " !\"#\$%&'()*+,-./")
+        GR.text(0.05, 0.80-y, "0123456789:;<=>?")
+        GR.text(0.05, 0.75-y, "@ABCDEFGHIJKLMNO")
+        GR.text(0.05, 0.70-y, "PQRSTUVWXYZ[\\]^_")
+        GR.text(0.05, 0.65-y, "`abcdefghijklmno")
+        GR.text(0.05, 0.60-y, "pqrstuvwxyz{|}~ ")
 
-GR.text(0.5, 0.85, " ¡¢£¤¥¦§¨©ª«¬­®¯")
-GR.text(0.5, 0.80, "°±²³´µ¶·¸¹º»¼½¾¿")
-GR.text(0.5, 0.75, "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏ")
-GR.text(0.5, 0.70, "ÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞß")
-GR.text(0.5, 0.65, "àáâãäåæçèéêëìíîï")
-GR.text(0.5, 0.60, "ðñòóôõö÷øùúûüýþÿ")
+        GR.text(0.5, 0.85-y, " ¡¢£¤¥¦§¨©ª«¬­®¯")
+        GR.text(0.5, 0.80-y, "°±²³´µ¶·¸¹º»¼½¾¿")
+        GR.text(0.5, 0.75-y, "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏ")
+        GR.text(0.5, 0.70-y, "ÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞß")
+        GR.text(0.5, 0.65-y, "àáâãäåæçèéêëìíîï")
+        GR.text(0.5, 0.60-y, "ðñòóôõö÷øùúûüýþÿ")
 
-GR.updatews()
+        GR.settextfontprec(233, 3)
+        y = 0.4
+    end
+    GR.updatews()
+end
+
+main()
