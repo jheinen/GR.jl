d6873c50fb47a83ae34567b93cc392e8d80368cd
Malte Deckers <m.deckers@fz-juelich.de>
2020-04-22 11:42:25 +0200

    [JSTerm] Replace Jupyter Comm by WebSockets
     * blocking communication
     * one WebSocket-server per running notebook
       * listening on different ports
     * add dependencies: JSON, HTTP
     * add reference-id mechanism for custom eventhandling

--- src/js.jl
+++ src/js.jl
@@ -2,6 +2,9 @@ module js
 
 import GR
 import UUIDs
+import HTTP
+import Sockets
+import JSON
 
 pxwidth = 640
 pxheight = 480
@@ -9,7 +12,8 @@ pxheight = 480
 id_count = 0
 js_running = false
 checking_js = false
-send_on_connect = Dict()
+conditions = Dict()
+l = ReentrantLock()
 
 
 mutable struct JSTermWidget
@@ -22,9 +26,9 @@ end
 
 
 function inject_js()
-  global comm
+  global wss, port
 
-  comm = nothing
+  wss = nothing
   _js_fallback = "https://gr-framework.org/downloads/gr-latest.js"
   _gr_js = if isfile(joinpath(ENV["GRDIR"], "lib", "gr.js"))
     _gr_js = try
@@ -42,6 +46,7 @@ function inject_js()
   else
       display(HTML(string("""
         <script type="text/javascript" id="jsterm-javascript">
+          WEB_SOCKET_ADDRESS = 'ws://127.0.0.1:""", port, """';
           if (typeof jsterm === 'undefined') {
               """, _gr_js, """
           }
@@ -50,54 +55,25 @@ function inject_js()
   end
 end
 
-check_comm = nothing
-
 function check_js()
-    global js_running, checking_js
+    global js_running, checking_js, port
     if !checking_js
         checking_js = true
-        @eval function Main.IJulia.register_comm(c::Main.IJulia.Comm, data)
-            global js_running, send_on_connect, checking_js, comm
-            if data.content["target_name"] == "check_comm"
-                testcomm = c
-                testcomm.on_msg = function(msg)
-                    if msg.content["data"]["inject_js"]
-                        inject_js()
-                    end
-                end
-                testcomm.on_close = function(msg)
-                    d = Dict("text/html"=>string(""))
-                    transient = Dict("display_id"=>"jsterm_check")
-                    Main.IJulia.send_ipython(Main.IJulia.publish[], Main.IJulia.msg_pub(Main.IJulia.execute_msg, "update_display_data", Dict("data"=>d, "metadata"=>Dict(), "transient"=>transient)))
-                    checking_js = false
-                end
-            elseif data.content["target_name"] == "jsterm_comm"
-                comm = c
-                js_running = true
-                comm.on_close = function comm_close_callback(msg)
-                  global js_running, comm
-                  js_running = false
-                  comm = nothing
-                end
-                comm.on_msg = comm_msg_callback
-                for disp in keys(send_on_connect)
-                    jsterm_send(send_on_connect[disp], disp)
-                end
-                send_on_connect = Dict()
-            end
-        end
         d = Dict("text/html"=>string("""
           <script type="text/javascript">
-            var check_comm = Jupyter.notebook.kernel.comm_manager.new_comm('check_comm');
-            check_comm.send({"inject_js": document.getElementById('jsterm-javascript') === null});
+            WEB_SOCKET_ADDRESS = 'ws://127.0.0.1:""", port, """';
             if (typeof JSTerm !== 'undefined') {
-                if (typeof jsterm === 'undefined') {
-                    jsterm = new JSTerm();
-                    console.log('jsterm created');
-                }
-                jsterm.registerComm();
+              if (typeof jsterm === 'undefined') {
+                jsterm = new JSTerm();
+              }
+              jsterm.connectWs();
+            } else {
+              ws = new WebSocket('ws://127.0.0.1:""", port, """');
+              ws.onopen = function() {
+                ws.send("inject-js");
+                ws.close();
+              }
             }
-            check_comm.close();
           </script>
         """))
         transient = Dict("display_id"=>"jsterm_check")
@@ -111,7 +87,7 @@ function JSTermWidget(id::Int, width::Int, height::Int, disp::Int)
   if GR.isijulia()
     id_count += 1
     if !js_running
-      check_js()
+      @async check_js()
     end
     JSTermWidget(id, width, height, disp, false)
   else
@@ -119,69 +95,86 @@ function JSTermWidget(id::Int, width::Int, height::Int, disp::Int)
   end
 end
 
-comm = nothing
+wss = nothing
 
-evthandler = Dict()
+evthandler = Dict{Int32,Any}()
 global_evthandler = nothing
 
 
-function register_evthandler(f::Function, device, port)
-  global evthandler
+function register_evthandler(f::Function, id)
+  global evthandler, l
   if GR.isijulia()
-    send_command(Dict("command" => "enable_events"), "cmd", string(device, port))
-    evthandler[string(device, port)] = f
+    send_command(Dict("command" => "enable_events"), "cmd", id)
+    lock(l) do
+      evthandler[id] = f
+    end
   else
     error("register_evthandler is only available in IJulia environments")
   end
 end
 
-function unregister_evthandler(device, port)
-  global evthandler
+function unregister_evthandler(id)
+  global evthandler, l
   if GR.isijulia()
     if global_evthandler === nothing
-      send_command(Dict("command" => "disable_events"), "cmd", string(device, port))
+      send_command(Dict("command" => "disable_events"), "cmd", id)
     end
-    evthandler[string(device, port)] = nothing
+    lock(l) do
+      evthandler[id] = nothing
+    end
   else
     error("unregister_evthandler is only available in IJulia environments")
   end
 end
 
 function register_evthandler(f::Function)
-  global global_evthandler
+  global global_evthandler, l
   if GR.isijulia()
     send_command(Dict("command" => "enable_events"), "cmd", nothing)
-    global_evthandler = f
+    lock(l) do
+      evthandler[id] = f
+    end
   else
     error("register_evthandler is only available in IJulia environments")
   end
 end
 
 function unregister_evthandler()
-  global global_evthandler, evthandler
+  global global_evthandler, evthandler, l
   if GR.isijulia()
     send_command(Dict("command" => "disable_events"), "cmd", nothing)
-    for key in keys(evthandler)
-      if evthandler[key] !== nothing
-        send_command(Dict("command" => "enable_events"), "cmd", key)
+    lock(l) do
+      for key in keys(evthandler)
+        if evthandler[key] !== nothing
+          send_command(Dict("command" => "enable_events"), "cmd", key)
+        end
       end
+      evthandler[id] = nothing
     end
-    global_evthandler = nothing
   else
     error("unregister_evthandler is only available in IJulia environments")
   end
 end
 
 function send_command(msg, msgtype, id=nothing)
-  global comm
+  global wss, ws
   if GR.isijulia()
-    if comm === nothing
-      error("JSTerm comm not initialized.")
+    if id !== nothing
+      m = merge(msg, Dict("type" => msgtype, "id" => id))
     else
-      if id !== nothing
-        Main.IJulia.send_comm(comm, merge(msg, Dict("type" => msgtype, "id" => id)))
-      else
-        Main.IJulia.send_comm(comm, merge(msg, Dict("type" => msgtype)))
+      m = merge(msg, Dict("type" => msgtype))
+    end
+    if !js_running
+      conditions["sendonconnect"] = Condition()
+      @async check_js()
+      wait(conditions["sendonconnect"])
+    end
+    if ws != nothing
+      try
+        HTTP.write(ws, Array{UInt8}(JSON.json(m)))
+      catch e
+        ws = nothing
+        js_running = false
       end
     end
   else
@@ -189,33 +182,25 @@ function send_command(msg, msgtype, id=nothing)
   end
 end
 
-function send_evt(msg, device, port)
+function send_evt(msg, id)
   if GR.isijulia()
-    send_command(msg, "evt", string(device, port))
+    send_command(msg, "evt", id)
   else
     error("send_evt is only available in IJulia environments")
   end
 end
 
-function send_evt(msg, identifier)
+function disable_jseventhandling(id)
   if GR.isijulia()
-    send_command(msg, "evt", identifier)
-  else
-    error("send_evt is only available in IJulia environments")
-  end
-end
-
-function disable_jseventhandling(device, port)
-  if GR.isijulia()
-    send_command(Dict("command" => "disable_jseventhandling"), "cmd", string(device, port))
+    send_command(Dict("command" => "disable_jseventhandling"), "cmd", id)
   else
     error("disable_jseventhandling is only available in IJulia environments")
   end
 end
 
-function enable_jseventhandling(device, port)
+function enable_jseventhandling(id)
   if GR.isijulia()
-    send_command(Dict("command" => "enable_jseventhandling"), "cmd", string(device, port))
+    send_command(Dict("command" => "enable_jseventhandling"), "cmd", id)
   else
     error("enable_jseventhandling is only available in IJulia environments")
   end
@@ -230,55 +215,79 @@ function disable_jseventhandling()
 end
 
 function enable_jseventhandling()
+  global ws
   if GR.isijulia()
     send_command(Dict("command" => "enable_jseventhandling"), "cmd", nothing)
   else
-    Main.IJulia.send_comm(comm, Dict("json" => f, "type" => "evt"))
     error("enable_jseventhandling is only available in IJulia environments")
   end
 end
 
 function comm_msg_callback(msg)
-  data = msg.content["data"]
+  global conditions, evthandler, l
+  data = msg
   if haskey(data, "type")
     if data["type"] == "save"
       d = Dict("text/html"=>string("<script type=\"text/javascript\" class=\"jsterm-data-widget\">", data["content"]["data"]["widget_data"], "</script>"))
       transient = Dict("display_id"=>string("save_display_", data["display_id"]))
       Main.IJulia.send_ipython(Main.IJulia.publish[], Main.IJulia.msg_pub(Main.IJulia.execute_msg, "update_display_data", Dict("data"=>d, "metadata"=>Dict(), "transient"=>transient)))
     elseif data["type"] == "evt"
-      global_evthandler(data["content"])
-      if haskey(evthandler, data["id"]) && evthandler[data["id"]] !== nothing
-        evthandler[data["id"]](data["content"])
+      lock(l) do
+        if haskey(evthandler, data["id"]) && evthandler[data["id"]] !== nothing
+          evthandler[data["id"]](data["content"])
+        end
       end
+    elseif data["type"] == "value"
+
+    elseif data["type"] == "createDisplay"
+      d = Dict("text/html"=>"")
+      transient = Dict("display_id"=>string("save_display_", data["dispid"]))
+      Main.IJulia.send_ipython(Main.IJulia.publish[], Main.IJulia.msg_pub(Main.IJulia.execute_msg, "display_data", Dict("data"=>d, "metadata"=>Dict(), "transient"=>transient)))
+      display(HTML(string("<div style=\"display: none;\" id=\"jsterm-display-", data["dispid"], "\">")))
+    elseif data["type"] == "ack"
+      notify(conditions[data["dispid"]])
     end
   end
 end
 
 function jsterm_send(data::String, disp)
-  global js_running, draw_end_condition, comm
+  global js_running, draw_end_condition, ws, conditions
   if GR.isijulia()
-    Main.IJulia.send_comm(comm, Dict("json" => data, "type"=>"draw", "display"=>disp))
+    if !js_running
+      conditions["sendonconnect"] = Condition()
+      @async check_js()
+      wait(conditions["sendonconnect"])
+    end
+      if ws != nothing
+        try
+          conditions[disp] = Condition()
+          HTTP.write(ws, Array{UInt8}(JSON.json(Dict("json" => data, "type"=>"draw", "display"=>disp))))
+          wait(conditions[disp])
+        catch e
+          ws = nothing
+          js_running = false
+        end
+      end
   else
     error("jsterm_send is only available in IJulia environments")
   end
 end
 
+function get_prev_plot_id()
+  send_command(Dict("value"=>"prev_id"), "inq")
+end
+
+function set_ref_id(id::Int)
+  send_command(Dict("id"=>id), "set_ref_id")
+end
+
 function recv(name::Cstring, id::Int32, msg::Cstring)
     # receives string from C and sends it to JS via Comm
-    global js_running, draw_end_condition, send_on_connect
+    global js_running, draw_end_condition
 
-    disp = string(UUIDs.uuid4());
+    id = string(UUIDs.uuid4());
 
-    d = Dict("text/html"=>"")
-    transient = Dict("display_id"=>string("save_display_", disp))
-    Main.IJulia.send_ipython(Main.IJulia.publish[], Main.IJulia.msg_pub(Main.IJulia.execute_msg, "display_data", Dict("data"=>d, "metadata"=>Dict(), "transient"=>transient)))
-    display(HTML(string("<div style=\"display: none;\" id=\"jsterm-display-", disp, "\">")))
-    if !js_running
-      check_js()
-      send_on_connect[disp] = unsafe_string(msg)
-    else
-        jsterm_send(unsafe_string(msg), disp)
-    end
+    jsterm_send(unsafe_string(msg), id)
     return convert(Int32, 1)
 end
 
@@ -287,20 +296,66 @@ function send(name::Cstring, id::Int32)
     return convert(Cstring, "String")
 end
 
-jswidgets = nothing
 send_c = nothing
 recv_c = nothing
 init = false
 
+function ws_cb(webs)
+  global comm_msg_callback, ws, js_running, conditions, checking_js
+  check = false
+  while !eof(webs)
+    data = readavailable(webs)
+    if length(data) > 0
+      data = String(data)
+      if data == "inject-js"
+        check = true
+        inject_js()
+        d = Dict("text/html"=>string(""))
+        transient = Dict("display_id"=>"jsterm_check")
+        Main.IJulia.send_ipython(Main.IJulia.publish[], Main.IJulia.msg_pub(Main.IJulia.execute_msg, "update_display_data", Dict("data"=>d, "metadata"=>Dict(), "transient"=>transient)))
+        checking_js = false
+      elseif data == "js-running"
+        d = Dict("text/html"=>string(""))
+        transient = Dict("display_id"=>"jsterm_check")
+        Main.IJulia.send_ipython(Main.IJulia.publish[], Main.IJulia.msg_pub(Main.IJulia.execute_msg, "update_display_data", Dict("data"=>d, "metadata"=>Dict(), "transient"=>transient)))
+        ws = webs
+        js_running = true
+        if haskey(conditions, "sendonconnect")
+          notify(conditions["sendonconnect"])
+        end
+      else
+        comm_msg_callback(JSON.parse(data))
+      end
+    end
+  end
+  if !check
+    ws = nothing
+    js_running = false
+    @async check_js()
+  end
+end
+
 function initjs()
-    global jswidgets, send_c, recv_c, init
+    global send_c, recv_c, init, checking_js, port, connect_cond, connected
 
     if !init && GR.isijulia()
-        init = true
-        jswidgets = Dict{Int32, JSTermWidget}()
-        send_c = @cfunction(send, Cstring, (Cstring, Int32))
-        recv_c = @cfunction(recv, Int32, (Cstring, Int32, Cstring))
-        check_js()
+      init = true
+      send_c = @cfunction(send, Cstring, (Cstring, Int32))
+      recv_c = @cfunction(recv, Int32, (Cstring, Int32, Cstring))
+      connect_cond = Condition()
+      connected = false
+      ws_server_task = @async begin
+        global port, connect_cond, connected
+        port, server = Sockets.listenany(8081)
+        @async HTTP.listen(server=server) do webs
+          HTTP.WebSockets.upgrade(ws_cb, webs)
+        end
+        connected = true
+        notify(connect_cond)
+      end
+      if !connected
+        wait(connect_cond)
+      end
     end
     send_c, recv_c
 end
