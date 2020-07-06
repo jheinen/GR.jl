60952721c47d60a465ba6f5505ef6abe0354aef1
Josef Heinen <j.heinen@fz-juelich.de>
2020-07-06 07:57:13 +0200

    Use the given Julia program, if specified

--- examples/make-sysimg.sh
+++ examples/make-sysimg.sh
@@ -9,4 +9,4 @@ fi
 ${julia} -e 'using PackageCompiler; create_sysimage(:GR; precompile_execution_file=joinpath(pwd(), "snoop.jl"), sysimage_path=joinpath(pwd(), "sys"))' >/dev/null
 
 echo To use the new system image, please start Julia with the -J option:
-echo julia -J sys
+echo ${julia} -J sys
