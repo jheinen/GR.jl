8d531b0d8bd88a6ae7aa61be6d5bf6d4ddca7785
Josef Heinen <j.heinen@fz-juelich.de>
2020-11-14 18:32:59 +0100

    Added another Pluto example

---
+++ examples/covid-19.jl
@@ -0,0 +1,73 @@
+### A Pluto.jl notebook ###
+# v0.12.10
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        el
+    end
+end
+
+# ╔═╡ e2d1cb38-2698-11eb-2e59-632faa201d6f
+begin
+    using CSV, DataFrames
+	using PlutoUI
+
+	ENV["GRDISPLAY"] = "pluto"
+    using GR
+	
+    GR.js.init_pluto()
+end
+
+# ╔═╡ 49d120ea-2699-11eb-203d-fddbb33bb08b
+begin
+	url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+	download(url, "covid-19.csv")
+end;
+
+# ╔═╡ 4b5f800a-2699-11eb-2528-b5917c0461cf
+begin
+	data = CSV.File("covid-19.csv") |> DataFrame
+    ncountries, ncols = size(data)
+    ndays = ncols - 4
+end;
+
+# ╔═╡ 1c4aec86-269a-11eb-2204-098352e8267c
+@bind countries MultiSelect(["Germany", "Austria", "Belgium", "Netherlands", "France", "Italy", "Spain", "US"], default=["Germany"])
+
+# ╔═╡ 75ad9770-2699-11eb-0db7-5948da39d6e1
+begin
+	cummulated = Dict()
+	for i in 1:ncountries
+		country = data[i,2]
+		if country in countries
+			if !haskey(cummulated, country) cummulated[country] = zeros(ndays) end
+			cummulated[country] .+= collect(data[i,5:end])
+		end
+	end
+end
+
+# ╔═╡ 86c70334-2699-11eb-1b52-cfa50b314e2b
+begin
+	day = collect(Float64, 1:ndays);
+	confirmed = hcat([cummulated[country] for country in countries]...)
+end;
+
+# ╔═╡ 918f640a-2699-11eb-02d8-998ed76c614a
+plot(day, confirmed, xlim=(0, ndays+1), ylim=(10, 12000000), ylog=true,
+     title="Confirmed SARS–CoV–2 infections", xlabel="Day", ylabel="Confirmed",
+     labels=countries, location=4)
+
+# ╔═╡ Cell order:
+# ╠═e2d1cb38-2698-11eb-2e59-632faa201d6f
+# ╠═49d120ea-2699-11eb-203d-fddbb33bb08b
+# ╠═4b5f800a-2699-11eb-2528-b5917c0461cf
+# ╠═75ad9770-2699-11eb-0db7-5948da39d6e1
+# ╠═86c70334-2699-11eb-1b52-cfa50b314e2b
+# ╠═1c4aec86-269a-11eb-2204-098352e8267c
+# ╠═918f640a-2699-11eb-02d8-998ed76c614a
