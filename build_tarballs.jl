12a4da83667a94b9713df2afff430e67a87b264b
Josef Heinen <j.heinen@fz-juelich.de>
2021-02-25 17:43:45 +0100

    Bump GR version

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,7 +1,7 @@
 using BinaryBuilder
 
 name = "GR"
-version = v"0.54.0"
+version = v"0.55.0"
 
 # Collection of sources required to build gr
 # qt5-runtime-Windows-x86_64.tar.gz was included to provide headers for Qt 5.12
