fe9228ddc7d73ac7850644f681dc240a78293404
Josef Heinen <j.heinen@fz-juelich.de>
2021-05-19 16:03:19 +0200

    Added an in-memory rendering example

---
+++ examples/image_mem_ex.jl
@@ -0,0 +1,33 @@
+ENV["GKS_WSTYPE"] = "100"
+
+using GR
+using Printf
+using BenchmarkTools
+
+w, h = (800, 600)
+
+image = rand(UInt8, 4, w, h)  # allocate the memory 
+mem = Printf.@sprintf("%p", pointer(image))
+conid = Printf.@sprintf("!%dx%d@%s.mem",  w, h, mem[3:end])
+
+@btime begin
+    beginprint(conid)
+    plot(rand(1000), size=(w, h))
+    endprint()
+end
+
+emergencyclosegks()
+
+delete!(ENV, "GKS_WSTYPE") 
+
+mwidth, mheight, width, height = inqdspsize()
+size = 400 * mwidth / width
+
+setwsviewport(0, size, 0, 3/4 * size)
+setwswindow(0, 1, 0, 3/4)
+
+setviewport(0, 1, 0, 3/4)
+setwindow(0, 1, 0, 3/4)
+
+drawimage(0, 1, 0, 3/4, w, h, reshape(reinterpret(UInt32, image), w, h))
+updatews()
