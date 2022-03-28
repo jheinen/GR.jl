9654541bc0fdf4e731519cf484f8158952d20119
Josef Heinen <j.heinen@fz-juelich.de>
2022-03-28 13:41:56 +0200

    Bump GR version

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -9,13 +9,13 @@
 using BinaryBuilder
 
 name = "GR"
-version = v"0.64.0"
+version = v"0.64.1"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/sciapp/gr.git", "08c185a6ee3fc0ceaebb9b92017def13131bff22"),
+    GitSource("https://github.com/sciapp/gr.git", "41ed7a59d79ded65e603084a021e27049c251dae"),
     FileSource("https://github.com/sciapp/gr/releases/download/v$version/gr-$version.js",
-               "5d5f80104639a51e40b8fc35b741f5a12291f90c33cfd4d90075ecbd393352e2", "gr.js")
+               "3b4105772ac0b41a4cb2fb926afd37c9b1c1eabdadb77aa332da642361aeeeb8", "gr.js")
 ]
 
 # Bash recipe for building across all platforms
