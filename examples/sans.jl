2aff3f6c8e7fc7b0b50144b809483874f3f00d1b
Josef Heinen <j.heinen@fz-juelich.de>
2022-09-22 10:33:21 +0200

    Add header in SANS plot example

--- examples/sans.jl
+++ examples/sans.jl
@@ -3,7 +3,7 @@ using GR
 using DelimitedFiles
 
 function main()
-    z = Z = readdlm("sans.dat")
+    z = Z = readdlm("sans.dat", skipstart=3)
     G = [ exp(-x^2 -y^2) for x in LinRange(-1.5, 1.5, 128), y in LinRange(-1.5, 1.5, 128) ]
 
     for t = 0:500
