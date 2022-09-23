e4e9d06b4959468b149f48f4ae7b01ca628f3fdf
t-bltg <tf.bltg@gmail.com>
2022-09-23 11:05:17 +0200

    add debug message in case of failure

--- GRPreferences/src/GRPreferences.jl
+++ GRPreferences/src/GRPreferences.jl
@@ -2,7 +2,8 @@ module GRPreferences
     using Preferences
     try
         import GR_jll
-    catch
+    catch err
+        @debug "import GR_jll failed" err
     end
 
     const os = Sys.KERNEL === :NT ? :Windows : Sys.KERNEL
@@ -27,36 +28,36 @@ module GRPreferences
         binary = @load_preference("binary", haskey(ENV, "GRDIR") ? "system" : "GR_jll")
         if binary == "GR_jll"
             grdir[]   = GR_jll.find_artifact_dir()
+            gksqt[]   = GR_jll.gksqt_path
             libGR[]   = GR_jll.libGR
             libGR3[]  = GR_jll.libGR3
             libGRM[]  = GR_jll.libGRM
-            gksqt[]   = GR_jll.gksqt_path
             libpath[] = GR_jll.LIBPATH[]
         elseif binary == "system"
             grdir[]   = haskey(ENV, "GRDIR") ? ENV["GRDIR"] : @load_preference("grdir")
+            gksqt[]   = joinpath(grdir[], "bin", "gksqt" * (os === :Windows ? ".exe" : ""))
             libGR[]   = lib_path(grdir[], "libGR")
             libGR3[]  = lib_path(grdir[], "libGR3")
             libGRM[]  = lib_path(grdir[], "libGRM")
-            gksqt[]   = joinpath(grdir[], "bin", "gksqt" * (os === :Windows ? ".exe" : ""))
             libpath[] = joinpath(grdir[], "lib")
         else
             error("Unknown GR binary: $binary")
         end
     end
 
-    use_system_binary(grdir; export_prefs = false, force = false) =
-        set_preferences!(GRPreferences,
-            "binary" => "system",
-            "grdir" => grdir,
-            export_prefs = export_prefs,
-            force = force
-        )
+    use_system_binary(grdir; export_prefs = false, force = false) = set_preferences!(
+        GRPreferences,
+        "binary" => "system",
+        "grdir" => grdir,
+        export_prefs = export_prefs,
+        force = force
+    )
 
-    use_jll_binary(; export_prefs = false, force = false) =
-        set_preferences!(GRPreferences,
-            "binary" => "GR_jll",
-            "grdir" => nothing,
-            export_prefs = export_prefs,
-            force = force
-        )
+    use_jll_binary(; export_prefs = false, force = false) = set_preferences!(
+        GRPreferences,
+        "binary" => "GR_jll",
+        "grdir" => nothing,
+        export_prefs = export_prefs,
+        force = force
+    )
 end
