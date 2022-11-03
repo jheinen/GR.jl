833468ea97bc558434317223e56cf7ba51d73a4f
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2022-11-02 22:57:37 -0400

    Fix svgplugin.dll issues on Windows

--- src/funcptrs.jl
+++ src/funcptrs.jl
@@ -33,6 +33,8 @@ function load_libs(always::Bool = false)
     libGR_handle[]  = Libdl.dlopen(GRPreferences.libGR[])
     libGR3_handle[] = Libdl.dlopen(GRPreferences.libGR3[])
     libGRM_handle[] = Libdl.dlopen(GRPreferences.libGRM[])
+    # Fix svgplugin.dll loading issues on Windows, why?
+    Libdl.dlopen(joinpath(GRPreferences.libpath[], "svgplugin"))
     lp = GRPreferences.libpath[]
     if Sys.iswindows()
         ENV["PATH"] = join((lp, get(ENV, "PATH", "")), ';')
