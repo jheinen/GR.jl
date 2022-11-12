82a75b7612712f15dd8227a5875551729b1ee573
t-bltg <tf.bltg@gmail.com>
2022-11-12 15:03:32 +0100

    do no use https for macos

--- test/downstream_test.jl
+++ test/downstream_test.jl
@@ -8,12 +8,13 @@ Plots_jl = joinpath(mkpath(tempname()), "Plots.jl")
 # clone and checkout the latest stable version of Plots
 depot = joinpath(first(DEPOT_PATH), "registries", "General", "P", "Plots", "Versions.toml")
 stable = maximum(VersionNumber.(keys(TOML.parse(read(depot, String)))))
-repo = Pkg.GitTools.ensure_clone(stdout, Plots_jl, "https://github.com/JuliaPlots/Plots.jl")
+# don't use the `https` protocol for `macOS`
+repo = Pkg.GitTools.ensure_clone(stdout, Plots_jl, "http://github.com/JuliaPlots/Plots.jl")
 tag = LibGit2.GitObject(repo, "v$stable")
 hash = string(LibGit2.target(tag))
 LibGit2.checkout!(repo, hash)
 
-# fake the suported GR version for testing
+# fake the supported GR version for testing (for `Pkg.develop`)
 Plots_toml = joinpath(Plots_jl, "Project.toml")
 toml = TOML.parse(read(Plots_toml, String))
 toml["compat"]["GR"] = GR.version()  
@@ -21,7 +22,7 @@ open(Plots_toml, "w") do io
   TOML.print(io, toml)
 end
 Pkg.develop(path=Plots_jl)
-Pkg.status(["Plots", "GR"])
+Pkg.status(["GR", "Plots"])
 
 # test basic plots creation and bitmap or vector exports
 using Plots, Test
@@ -29,9 +30,9 @@ using Plots, Test
 prefix = tempname()
 @time for i ∈ 1:length(Plots._examples)
   i ∈ Plots._backend_skips[:gr] && continue  # skip unsupported examples
-  Plots._examples[i].imports === nothing || continue  # skip examples requiring optional test deps
+  Plots._examples[i].imports ≡ nothing || continue  # skip examples requiring optional test deps
   pl = Plots.test_examples(:gr, i; disp = false)
-  for ext in (".png", ".pdf")
+  for ext in (".png", ".pdf")  # TODO: maybe more ?
     fn = string(prefix, i, ext)
     Plots.savefig(pl, fn)
     @test filesize(fn) > 1_000
