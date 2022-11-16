c0f5b22e9d399be623c46eda6e61bbba6752422c
Josef Heinen <j.heinen@fz-juelich.de>
2022-11-16 11:32:15 +0100

    Add Plots test script

---
+++ examples/test-Plots.jl
@@ -0,0 +1,17 @@
+using Plots, Test
+
+@info("Interactive tests")
+Plots.test_examples(:gr)
+
+@info("Figure output tests")
+prefix = tempname()
+@time for i ∈ 1:length(Plots._examples)
+  i ∈ Plots._backend_skips[:gr] && continue  # skip unsupported examples
+  Plots._examples[i].imports ≡ nothing || continue  # skip examples requiring optional test deps
+  pl = Plots.test_examples(:gr, i; disp = false)
+  for ext in (".png", ".pdf", ".svg")
+    fn = string(prefix, i, ext)
+    Plots.savefig(pl, fn)
+    @test filesize(fn) > 1_000
+  end
+end
