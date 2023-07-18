c1f5d538fe101d6bfbb664501cb9ec43dcad25bf
Josef Heinen <j.heinen@fz-juelich.de>
2023-07-18 10:24:29 +0200

    release 0.72.9

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder
 
 name = "GR"
-version = v"0.72.8"
+version = v"0.72.9"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/sciapp/gr.git", "2e9d75e3e062ac2dc3e520456e4769901e24cd86"),
+    GitSource("https://github.com/sciapp/gr.git", "623b90911a3fc68b253ae26fb0f259ca97d8df7d"),
     FileSource("https://github.com/sciapp/gr/releases/download/v$version/gr-$version.js",
                "55b6d9144b251124c85c8e72627496543e7f83a5d9fb543011331eaf0c41ff2a", "gr.js"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.14.sdk.tar.xz",
@@ -18,28 +18,21 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/gr
 
-if test -f "$prefix/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake"; then
-    sed -i 's/_qt5gui_find_extra_libs.*AGL.framework.*//' $prefix/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
-fi
-
 update_configure_scripts
 
 make -C 3rdparty/qhull -j${nproc}
 
 if [[ $target == *"mingw"* ]]; then
     winflags=-DCMAKE_C_FLAGS="-D_WIN32_WINNT=0x0f00"
-    tifflags=-DTIFF_LIBRARY=${libdir}/libtiff-5.dll
+    tifflags=-DTIFF_LIBRARY=${libdir}/libtiff-6.dll
 else
     tifflags=-DTIFF_LIBRARY=${libdir}/libtiff.${dlext}
 fi
 
 if [[ "${target}" == x86_64-apple-darwin* ]]; then
-    pushd $WORKSPACE/srcdir/MacOSX10.*.sdk
-    rm -rf /opt/${target}/${target}/sys-root/System
-    cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
-    cp -ra System "/opt/${target}/${target}/sys-root/."
+    apple_sdk_root=$WORKSPACE/srcdir/MacOSX10.14.sdk
+    sed -i "s!/opt/x86_64-apple-darwin14/x86_64-apple-darwin14/sys-root!$apple_sdk_root!" $CMAKE_TARGET_TOOLCHAIN
     export MACOSX_DEPLOYMENT_TARGET=10.14
-    popd
 fi
 
 if [[ "${target}" == *apple* ]]; then
@@ -102,16 +95,16 @@ dependencies = [
     Dependency("GLFW_jll"),
     Dependency("JpegTurbo_jll"),
     Dependency("libpng_jll"),
-    Dependency("Libtiff_jll"; compat="4.3.0"),
+    Dependency("Libtiff_jll"; compat="~4.5.1"),
     Dependency("Pixman_jll"),
-#    Dependency("Qhull_jll"),
-    Dependency("Qt5Base_jll"),
+    HostBuildDependency("Qt6Base_jll"),
+    Dependency("Qt6Base_jll"; compat="6.4.2"),
     BuildDependency("Xorg_libX11_jll"),
     BuildDependency("Xorg_xproto_jll"),
     Dependency("Zlib_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-# GCC version 7 because of ffmpeg, but building against Qt requires v8 on Windows.
+# GCC version 9 because of Qt6
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               preferred_gcc_version = v"8", julia_compat="1.6")
+               preferred_gcc_version = v"9", julia_compat="1.6")
