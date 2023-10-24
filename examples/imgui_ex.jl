e787dadb261206d4d93b7f6478fa55bb0de6606e
Josef Heinen <j.heinen@fz-juelich.de>
2023-10-24 14:01:50 +0200

    Update CImGui example

--- examples/imgui_ex.jl
+++ examples/imgui_ex.jl
@@ -1,24 +1,27 @@
-# This example does NOT work with recent CImGui versions (v1.82.0)
+# This example requires CImGui#master
 #
-# import Pkg; Pkg.add(name="CImGui", version="v1.78.0")
+# import Pkg; Pkg.add(name="CImGui", rev="master")
 #
 using CImGui
+using CImGui.ImGuiGLFWBackend
+using CImGui.ImGuiGLFWBackend.LibCImGui
+using CImGui.ImGuiGLFWBackend.LibGLFW
+using CImGui.ImGuiOpenGLBackend
+using CImGui.ImGuiOpenGLBackend.ModernGL
 using CImGui.CSyntax
 using CImGui.CSyntax.CStatic
-using CImGui.GLFWBackend
-using CImGui.OpenGLBackend
-using CImGui.GLFWBackend.GLFW
-using CImGui.OpenGLBackend.ModernGL
 using Printf
 
 using GR
 using LaTeXStrings
 
-const glsl_version = 150
-GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, 3)
-GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, 2)
-GLFW.WindowHint(GLFW.OPENGL_PROFILE, GLFW.OPENGL_CORE_PROFILE) # 3.2+ only
-GLFW.WindowHint(GLFW.OPENGL_FORWARD_COMPAT, GL_TRUE) # required on Mac
+glfwDefaultWindowHints()
+glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
+glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2)
+if Sys.isapple()
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE) # 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE) # required on Mac
+end
 
 function draw(phi)
     formulas = (
@@ -45,22 +48,22 @@ function draw(phi)
     updatews()
 end
 
-# setup GLFW error callback
-error_callback(err::GLFW.GLFWError) = @error "GLFW ERROR: code $(err.code) msg: $(err.description)"
-GLFW.SetErrorCallback(error_callback)
-
 # create window
-window = GLFW.CreateWindow(960, 720, "Demo")
+window = glfwCreateWindow(960, 720, "Demo", C_NULL, C_NULL)
 @assert window != C_NULL
-GLFW.MakeContextCurrent(window)
-GLFW.SwapInterval(1)  # enable vsync
+glfwMakeContextCurrent(window)
+glfwSwapInterval(1)  # enable vsync
+
+# create OpenGL and GLFW context
+window_ctx = ImGuiGLFWBackend.create_context(window)
+gl_ctx = ImGuiOpenGLBackend.create_context()
 
 # setup Dear ImGui context
 ctx = CImGui.CreateContext()
 
 # create texture for image drawing
 img_width, img_height = 500, 500
-image_id = ImGui_ImplOpenGL3_CreateImageTexture(img_width, img_height)
+image_id = ImGuiOpenGLBackend.ImGui_ImplOpenGL3_CreateImageTexture(img_width, img_height)
 
 ENV["GKS_WSTYPE"] = "100"
 image = rand(GLuint, img_width, img_height) # allocate the memory
@@ -68,16 +71,16 @@ mem = Printf.@sprintf("%p",pointer(image))
 conid = Printf.@sprintf("!%dx%d@%s.mem",  img_width, img_height, mem[3:end])
 
 # setup Platform/Renderer bindings
-ImGui_ImplGlfw_InitForOpenGL(window, true)
-ImGui_ImplOpenGL3_Init(glsl_version)
+ImGuiGLFWBackend.init(window_ctx)
+ImGuiOpenGLBackend.init(gl_ctx)
 
 try
     clear_color = Cfloat[0.45, 0.55, 0.60, 1.00]
-    while !GLFW.WindowShouldClose(window)
-        GLFW.PollEvents()
+    while glfwWindowShouldClose(window) == 0
+        glfwPollEvents()
         # start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame()
-        ImGui_ImplGlfw_NewFrame()
+        ImGuiOpenGLBackend.new_frame(gl_ctx)
+        ImGuiGLFWBackend.new_frame(window_ctx)
         CImGui.NewFrame()
 
         # show image example
@@ -88,31 +91,35 @@ try
             beginprint(conid)
             draw(phi * π/180)
             endprint()
-        end
 
-        ImGui_ImplOpenGL3_UpdateImageTexture(image_id, image, img_width, img_height)
-        CImGui.Image(Ptr{Cvoid}(image_id), (img_width, img_height))
-        CImGui.End()
+            ImGuiOpenGLBackend.ImGui_ImplOpenGL3_UpdateImageTexture(image_id, image, img_width, img_height)
+            CImGui.Image(Ptr{Cvoid}(image_id), CImGui.ImVec2(img_width, img_height))
+            CImGui.End()
+        end
 
         # rendering
         CImGui.Render()
-        GLFW.MakeContextCurrent(window)
-        display_w, display_h = GLFW.GetFramebufferSize(window)
+        glfwMakeContextCurrent(window)
+
+        width, height = Ref{Cint}(), Ref{Cint}() #! need helper fcn
+        glfwGetFramebufferSize(window, width, height)
+        display_w = width[]
+        display_h = height[]
+
         glViewport(0, 0, display_w, display_h)
         glClearColor(clear_color...)
         glClear(GL_COLOR_BUFFER_BIT)
-        ImGui_ImplOpenGL3_RenderDrawData(CImGui.GetDrawData())
+        ImGuiOpenGLBackend.render(gl_ctx)
 
-        GLFW.MakeContextCurrent(window)
-        GLFW.SwapBuffers(window)
+        glfwMakeContextCurrent(window)
+        glfwSwapBuffers(window)
     end
 catch e
     @error "Error in renderloop!" exception=e
     Base.show_backtrace(stderr, catch_backtrace())
 finally
-    ImGui_ImplOpenGL3_Shutdown()
-    ImGui_ImplGlfw_Shutdown()
+    ImGuiOpenGLBackend.shutdown(gl_ctx)
+    ImGuiGLFWBackend.shutdown(window_ctx)
     CImGui.DestroyContext(ctx)
-    GLFW.HideWindow(window)
-    GLFW.DestroyWindow(window)
+    glfwDestroyWindow(window)
 end
